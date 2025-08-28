7496d392f84e721f518497301ebe7d186ab08fb6
Enzo Lima <olucca104@gmail.com>
2025-08-28 08:39:56 -0300

    Atualização.

--- src/main/java/br/com/mariojp/solid/srp/ReceiptService.java
+++ src/main/java/br/com/mariojp/solid/srp/ReceiptService.java
@@ -1,18 +1,10 @@
 package br.com.mariojp.solid.srp;
 
 public class ReceiptService {
-	private TaxCalculator taxCalculator = new TaxCalculator();
-	private ReceiptFormatter receiptFormatter = new ReceiptFormatter();
-
-	public void ReceiptService(TaxCalculator taxCalculator, ReceiptFormatter receiptFormatter) {
-		this.taxCalculator = taxCalculator;
-		this.receiptFormatter = receiptFormatter;
-	}
-
 	public String generate(Order order) {
 		double subtotal = order.getItems().stream().mapToDouble(i -> i.getUnitPrice() * i.getQuantity()).sum();
-		double tax = TaxCalculator.calculator(subtotal);
+		double tax = new TaxCalculator().calculator(subtotal);
 		double total = subtotal + tax;
-		return receiptFormatter.formatter(order, subtotal, total, tax);
+		return  new ReceiptFormatter().formatter(order, subtotal, total, tax);
 	}
 }
--- src/main/java/br/com/mariojp/solid/srp/TaxCalculator.java
+++ src/main/java/br/com/mariojp/solid/srp/TaxCalculator.java
@@ -1,7 +1,7 @@
 package br.com.mariojp.solid.srp;
 
 public class TaxCalculator {
-    public static double calculator (double subtotal){
+    public double calculator (double subtotal){
         String tax = System.getProperty("tax.rate");
         if(tax == null){
             return subtotal * 0.03;
